7f6b03329977202e8c00b4cfdb11187a4fc1d48c
Treston <69865292+trestonbenton@users.noreply.github.com>
2023-06-23 23:37:37 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    // smooth scrolling
+    $('a[href^="#"]').on('click', function(event) {
+      var target = $(this.getAttribute('href'));
+      if (target.length) {
+        event.preventDefault();
+        $('html, body').stop().animate({
+          scrollTop: target.offset().top
+        }, 1000);
+      }
+    });
+  });
+  
